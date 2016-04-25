0c049a1bfbbfbcb1865208a0e4e35633d97bed73
Kaushik Mazumdar <writetokaushikmazumdar@gmail.com>
2016-04-25 10:04:53 +0530

    complete dining phil with proper thread id

--- semaphores/dining_phil.cc
+++ semaphores/dining_phil.cc
@@ -21,6 +21,7 @@ struct argStruct
 	sem_t semLeftFork;
 	sem_t semRightFork;
 	sem_t semEat;
+	sem_t semThink;
 	int tid;	
 };
 
@@ -34,10 +35,18 @@ int right_fork(int philNum)
 	return (philNum+1)%NUM_PHILOSOPHER;
 }
 
-void Think(int tid)
+void Think(void *arguments)
 {
-	cout << "Philosopher " << tid << " thinking...\n" ;
-	sleep(2);
+	struct argStruct *args = (struct argStruct*)arguments ;
+
+	if(sem_wait(&args->semThink)!=0)
+		exit(EXIT_FAILURE);
+
+	cout << "Philosopher " << args->tid << " thinking...\n" ;
+
+	if(sem_post(&args->semThink)!=0)
+		exit(EXIT_FAILURE);
+	sleep(1);
 }
 
 void Eat(void *arguments)
@@ -52,7 +61,7 @@ void Eat(void *arguments)
 		exit(EXIT_FAILURE);
 
 	cout << "Philosopher " << args->tid << " eating.\n";
-	sleep(1);
+	sleep(3);
 
 	if(sem_post(&args->semRightFork)!=0)
 		exit(EXIT_FAILURE);
@@ -65,12 +74,11 @@ void Eat(void *arguments)
 void *Philosopher(void *arguments)
 {
 	struct argStruct *args = (struct argStruct*)arguments ;
-	cout << args->tid << "\n";
 
 	for(int i=0; i<EAT_TIMES; i++)
 	{
-		//Think(args->tid);
-		//Eat(arguments);
+		Think(arguments);
+		Eat(arguments);
 	}
 
 	// exit the thread
@@ -82,6 +90,8 @@ int main()
 {
 	//semaphores
 	sem_t semPhil[NUM_PHILOSOPHER];
+	sem_t eat;
+	sem_t think;
 
 	//threads
 	pthread_t ptPhil[NUM_PHILOSOPHER];
@@ -92,7 +102,7 @@ int main()
 	cout << "Start operations...\n";
 
 	//populate arg
-	threadArg = (struct argStruct*)malloc(sizeof(struct argStruct));
+	threadArg = (struct argStruct*)malloc(sizeof(struct argStruct)*NUM_PHILOSOPHER);
 	if(threadArg==NULL)	
 		exit(EXIT_FAILURE);
 
@@ -102,17 +112,21 @@ int main()
 		if(sem_init(&semPhil[i],SEM_THREAD,1)!=0)
 			exit(EXIT_FAILURE);
 	}
-	if(sem_init(&threadArg->semEat,SEM_THREAD,NUM_PHILOSOPHER-1)!=0)
+	if(sem_init(&eat,SEM_THREAD,NUM_PHILOSOPHER-1)!=0)
+		exit(EXIT_FAILURE);
+	if(sem_init(&think,SEM_THREAD,1)!=0)
 		exit(EXIT_FAILURE);
 
 	//init threads
 	for(int i=0; i<NUM_PHILOSOPHER; i++)
 	{
-		threadArg->tid = i;
-		threadArg->semLeftFork = semPhil[left_fork(i)];
-		threadArg->semRightFork = semPhil[right_fork(i)];
+		threadArg[i].tid = i;
+		threadArg[i].semLeftFork = semPhil[left_fork(i)];
+		threadArg[i].semRightFork = semPhil[right_fork(i)];
+		threadArg[i].semEat = eat;
+		threadArg[i].semThink = think;
 
-		if(pthread_create(&ptPhil[i],NULL,Philosopher,(void*)threadArg)!=0)
+		if(pthread_create(&ptPhil[i],NULL,Philosopher,(void*)(&threadArg[i]))!=0)
 			exit(EXIT_FAILURE);
 	}
 
